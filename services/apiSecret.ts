ab2d11ed5ae9b0267761d8ea4376432a5e86ffd2
Philipp Counter <philipp@daheim11.de>
2021-10-08 14:48:28 +0200

    update secrets to work on server

--- services/apiSecret.ts
+++ services/apiSecret.ts
@@ -1,6 +1,6 @@
-export const apiSecret = '';
+export const apiSecret = process.env.apiSecret;
 
 export const twitchSecret = {
-    client : '',
-    secret : ''
+    client : process.env.twitchClient,
+    secret : process.env.twitchSecret
 };
